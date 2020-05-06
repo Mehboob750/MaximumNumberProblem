3d0ae4f869842c9684eab1f83654600a5e1d44b6
Mehboob750 <ms568203@gmail.com>
2020-05-07 04:08:59 +0530

    Added Given three float values finds the maximum value

--- src/main/java/com/genericexample/FindMaximumNumber.java
+++ src/main/java/com/genericexample/FindMaximumNumber.java
@@ -10,4 +10,13 @@ public class FindMaximumNumber {
         else
             return number3;
     }
+
+    public Float maximumValue(float number1, float number2, float number3) {
+        if (number1>number2 && number1>number3)
+            return number1;
+        else if (number2>number1 && number2>number3)
+            return number2;
+        else
+            return number3;
+    }
 }
--- src/test/java/com/genericexample/FindMaximumNumberTest.java
+++ src/test/java/com/genericexample/FindMaximumNumberTest.java
@@ -22,4 +22,23 @@ public class FindMaximumNumberTest {
         int maxNumber = findMaximumNumber.maximumValue(10,20,30);
         Assert.assertEquals(30,maxNumber);
     }
+
+    @Test
+    public void givenThreeFloat_MaxValueAT_FirstPosition_ShouldReturnSame() {
+        FindMaximumNumber findMaximumNumber = new FindMaximumNumber();
+        Float maxNumber = findMaximumNumber.maximumValue(30.30f,10.10f,20.20f);
+        Assert.assertEquals(java.util.Optional.of((Float)30.30f),java.util.Optional.of(maxNumber));
+    }
+    @Test
+    public void givenThreeFloat_MaxValueAT_SecondPosition_ShouldReturnSame() {
+        FindMaximumNumber findMaximumNumber = new FindMaximumNumber();
+        Float maxNumber = findMaximumNumber.maximumValue(10.10f,30.30f,20.20f);
+        Assert.assertEquals(java.util.Optional.of((Float)30.30f),java.util.Optional.of(maxNumber));
+    }
+    @Test
+    public void givenThreeFloat_MaxValueAT_ThirdPosition_ShouldReturnSame() {
+        FindMaximumNumber findMaximumNumber = new FindMaximumNumber();
+        Float maxNumber = findMaximumNumber.maximumValue(10.10f,20.20f,30.30f);
+        Assert.assertEquals(java.util.Optional.of((Float)30.30f),java.util.Optional.of(maxNumber));
+    }
 }
