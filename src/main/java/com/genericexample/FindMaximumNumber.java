06a644628381aebfb7f9351ae83c0004f03a0d5b
Mehboob750 <ms568203@gmail.com>
2020-05-07 06:08:35 +0530

    Added Print the maximum value using Generic method

--- src/main/java/com/genericexample/FindMaximumNumber.java
+++ src/main/java/com/genericexample/FindMaximumNumber.java
@@ -7,7 +7,12 @@ public class FindMaximumNumber<E extends Comparable<E>> {
     public E maximumValue(E... values) {
         Arrays.sort(values);
         int maximumValueIndex=values.length-1;
+        FindMaximumNumber.printMaximumValue(values[maximumValueIndex]);
         return values[maximumValueIndex];
+    }
+
+    private static <E extends Comparable<E>> void printMaximumValue(E value) {
+        System.out.println("Maximum Value is: "+value);
     }
 
 }
